cf37deac389d8ea37b985be686ae2e8627e7f0f4
echorebel <kemuri@kemuri.de>
2015-12-20 22:11:43 +0100

    done lesson3

--- Lesson3/src/main.cpp
+++ Lesson3/src/main.cpp
@@ -1,9 +1,21 @@
 #include <string>
 #include <iostream>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#ifdef __APPLE__
+	#include <SDL_image.h>
+#elif __linux__
+	#include <SDL2/SDL_image.h>
+#endif
 #include "log.h"
+#include "res_path.h"
+#include "cleanup.h"
 
+/**
+ * Load an image into a texture on the rendering device
+ * @param file The image file to load
+ * @param ren The renderer to load the texture onto
+ * @return the loaded texture, or nullptr if something went wrong.
+ */
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren)
 {
     SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
@@ -14,12 +26,116 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren)
     return texture;
 }
 
+/**
+ * Draw an SDL_Texture to and SDL_Renderer at position x, y, with some desired
+ * width and height
+ * @param tex The source texture we want to draw
+ * @param ren The renderer we want to draw to
+ * @param x The x coordinate to draw to
+ * @param y The y coordinate to draw to
+ * @param w The width of the texture to draw
+ * @param h The height of the texture to draw
+ */
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h)
+{
+	// Setup the destination rectangle to be at the position we want
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	dst.w = w;
+	dst.h = h;
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+
+/**
+ * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+ * the texture's width and height
+ * @param tex The source texture we want to draw
+ * @param ren The renderer we want to draw to
+ * @param x The x coordinate to draw to
+ * @param y The y coordinate to draw to
+ */
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y)
+{
+	int w, h;
+	SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+	renderTexture(tex, ren, x, y, w, h);
+}
+
 int main(int argc, char **argv)
 {
 	const int SCREEN_WIDTH = 640;
 	const int SCREEN_HEIGHT = 460;
 
-  const int TILE_SIZE = 40;
+  	const int TILE_SIZE = 40;
+	
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		logSDLError(std::cout, "SDL_INIT");
+		return 1;
+	}
+	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if(win == nullptr)
+	{
+		logSDLError(std::cout, "CreateWindow");
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if(ren == nullptr){
+		cleanup(win);
+		logSDLError(std::cout, "CreateRenderer");
+		SDL_Quit();
+		return 1;
+	}
+
+	// not necessary happens automatically but it will delay start 
+	// so it's better to have it under control
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG)
+	{
+		logSDLError(std::cout, "IMG_Init");
+		SDL_Quit();
+		return 1;
+	}
+
+	const std::string resPath = getResourcePath("lesson3");
+	SDL_Texture *background  = loadTexture(resPath + "background.png", ren);
+	SDL_Texture *image = loadTexture(resPath + "image.png", ren);
+	// make sure they both loaded
+	if(background == nullptr || image == nullptr)
+	{
+		cleanup(background, image, ren, win);
+		IMG_Quit();
+		SDL_Quit();
+		return 1;
+	}	
+
+	// determine how many tiles we'll need to fill the screen
+	int xTiles = SCREEN_WIDTH / TILE_SIZE;
+	int yTiles = SCREEN_HEIGHT / TILE_SIZE;
+
+	// draw the tiles by calculating their position
+	for(int i=0; i < xTiles * yTiles; i++)
+	{
+		int x = i % xTiles;
+		int y = i / xTiles;
+		renderTexture( 
+				background, ren, x * TILE_SIZE, y * TILE_SIZE,TILE_SIZE,TILE_SIZE);
+	}
+
+	int iW, iH;
+	SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+	int x = SCREEN_WIDTH / 2 - iW / 2;
+	int y = SCREEN_HEIGHT / 2 - iH / 2;
+	renderTexture(image, ren, x, y);
+
+	SDL_RenderPresent(ren);
+
+	SDL_Delay(5000);
+
+	cleanup(background, image, ren, win);
+	IMG_Quit();
+	SDL_Quit();
 
 	return 0;
 }
